19356d032586bc7ee3b0cf09ba1ba80c45200e44
paulineareola30 <paulineareola7@gmail.com>
2024-12-11 23:38:28 +0800

    Update urls.py

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.expense_list, name='expense_list'),
     path('add/', views.add_expense, name='add_expense'),
+    path('delete/<int:expense_id>/', views.delete_expense, name='delete_expense'),
 ]
